9aadcacb8ed9d9a1133806e4e58b37b47d32c398
Petrik Diak <diak@petrik.hu>
2024-10-11 15:33:43 +0200

    validáció

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Post, Body, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { OrderDto } from './order.dto';
+import { Response, response } from 'express';
 
 @Controller()
 export class AppController {
@@ -16,6 +18,26 @@ export class AppController {
   @Get('order')
   @Render('orderForm')
   orderForm(){
-    
+    return{
+      errors: []
+    }
+  }
+
+  @Post('order')
+  order(@Body() orderDto: OrderDto, @Res() response: Response){
+    const errors : string[] = [];
+    if(!orderDto.nev || !orderDto.termek || !orderDto.szaml_varos){
+      errors.push('Minden mezőt tölts ki!')
+    }
+
+    if(orderDto.kupon && /^[A-Z]{2}-\d{4}$/.test(orderDto.kupon)){
+      errors.push('A kuponkód nem megfelelő formátumú!')
+    }
+
+    if(errors.length > 0){
+      response.render('orderForm',{
+        errors
+      })
+    }
   }
 }
